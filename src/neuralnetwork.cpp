bc9e385017a824473f1fbc3d4e696bda2a3ff759
root <root@DESKTOP-50563PA.localdomain>
2024-12-09 14:00:14 +0100

    done Layer class, Networkclass needs to be finished

--- src/neuralnetwork.cpp
+++ src/neuralnetwork.cpp
@@ -23,5 +23,47 @@ class Neuron{
         double relu_derivative(){
             return (value > 0.0) ? 1.0 : 0.0;
         }
+};
 
+class Layer{
+    private:
+        std::vector<std::shared_ptr<Neuron>> neurons;
+    
+    public:
+        Layer(int size){
+            for (int i = 0; i < size; i++){
+                this->neurons.push_back(std::make_shared<Neuron>());
+            }    
+        }
+};
+
+class Network{
+    private:
+        std::vector<std::shared_ptr<Layer>> layers;
+        std::vector<std::vector<double>> weights; // matrix form
+        std::vector<std::vector<double>> biases; // matrix form
+
+    public:
+        Network(const std::vector<int>& layer_sizes) { // Rename the parameter to `layer_sizes`
+            // Create layers
+            for (int size : layer_sizes) {
+                this->layers.push_back(std::make_shared<Layer>(size));
+            }
+
+            // Initialize weights and biases
+            // push a 2D vector into weights(1D)
+        }
+
+        void forward(){
+
+        }
+
+        void backpropagation(){
+
+        }
+
+        void update(){
+
+        }
+        
 };
